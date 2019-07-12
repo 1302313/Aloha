041d478747c9f38fe38502df98fb4682eb1a07b1
AdhamElNewihy <adham.el.newihy@gmail.com>
2019-07-11 19:52:48 -0700

    Updated html with classes, added styling to Section 5 - Subscription form design, explored JS

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // all of your JS code goes here
+    let magic = prompt ("Please type a number from 1 to 10");
+    alert("Thank you!!")
+  });
+
+
+  // add notifiction display:flex when cart is marked
