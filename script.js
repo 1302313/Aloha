429de94ddd796e295e172366e0f4e1bb603b3711
AdhamElNewihy <adham.el.newihy@gmail.com>
2019-07-14 21:03:59 -0700

    Added font styling according to requirements

--- script.js
+++ script.js
@@ -9,40 +9,41 @@ document.addEventListener("DOMContentLoaded", function () {
   // Smooth Scrolling to sections
   // Make sure to add Jquery link to html
   /* Reference: https://www.w3schools.com/howto/howto_css_smooth_scroll.asp */
+  /* Reference: https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
 
-    // Add the smooth scrolling to all links
-    // Select all links with hashes
-    $('a[href*="#"]')
+  // Add the smooth scrolling to all links
+  // Select all links with hashes
+  $('a[href*="#"]')
     //remove useless links :D 
     .not('[href="#"]')
     .not('[href="#0"]')
-    $("a").on('click', function (event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        // Use jQuery Function: animate() to add smooth scrolling
-        // The number below identifies the time (in milliseconds) for scrolling
-        // Identify the HTML Element to animate()
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 600, function () {
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        })
-      }
-    });
-    
-    // Section 4 - Most Loved Products (Carousel movement)
+  $("a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      // Use jQuery Function: animate() to add smooth scrolling
+      // The number below identifies the time (in milliseconds) for scrolling
+      // Identify the HTML Element to animate()
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 600, function () {
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      })
+    }
+  });
 
-    const elem = document.querySelector('.carousel');
-    const flkty = new Flickity(elem, {
+  // Section 4 - Most Loved Products (Carousel movement)
 
-      // options
-      cellAlign: 'left',
-      wrapAround: true,
-      freeScroll: true,
-      autoPlay: true,
-    })
+  const elem = document.querySelector('.carousel');
+  const flkty = new Flickity(elem, {
+
+    // options
+    cellAlign: 'left',
+    wrapAround: true,
+    freeScroll: true,
+    autoPlay: true,
+  })
 
   //   const messageForm = document.getElementById("email-bar");
   //   const input = document.getElementById("signup-email");
@@ -62,4 +63,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
   // add notifiction display:flex when cart is marked
-  });
+});
